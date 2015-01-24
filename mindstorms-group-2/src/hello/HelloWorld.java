7626d2f5247f4387158c1f1d967581ef43258713
AlSzacrel <al@alszacrel.de>
2015-01-24 15:36:30 +0100

    Baum!

--- mindstorms-group-2/src/hello/HelloWorld.java
+++ mindstorms-group-2/src/hello/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World! Here is Wei");
+		System.out.println("Hello World! And here is Mark");
 	}
 
 }
