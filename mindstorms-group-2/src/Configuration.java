856e4f30dda2f3a1bb01eace53d702b7505ac414
lars <briem.lars@googlemail.com>
2015-01-27 16:24:42 +0100

    Fixed compilation errors

--- mindstorms-group-2/src/Configuration.java
+++ mindstorms-group-2/src/Configuration.java
@@ -1,21 +1,23 @@
-import java.util.LinkedList;
-
 import lejos.nxt.Button;
 import lejos.nxt.LCD;
 import lejos.nxt.LightSensor;
+import lejos.nxt.Motor;
+import lejos.nxt.NXTRegulatedMotor;
 import lejos.nxt.SensorPort;
 
 public class Configuration {
 
-    private LightSensor light;
-    private final LinkedList<Step> steps;
+    private final LightSensor light;
+    private final NXTRegulatedMotor leftWheel;
+    private final NXTRegulatedMotor rightWheel;
+    private final NXTRegulatedMotor sensorMotor;
 
     public Configuration() {
         super();
         light = new LightSensor(SensorPort.S4);
-	leftWheel = Motor.A;
-	rightWheel = Motor.B;
-	sensorMotor = Motor.C;
+        leftWheel = Motor.A;
+        rightWheel = Motor.B;
+        sensorMotor = Motor.C;
     }
 
     public LightSensor getLight() {
@@ -35,17 +37,6 @@ public class Configuration {
         LCD.drawInt(light.getNormalizedLightValue(), 4, 0, 1);
         LCD.drawInt(SensorPort.S1.readRawValue(), 4, 0, 2);
         LCD.drawInt(SensorPort.S1.readValue(), 4, 0, 3);
-    }
-
-    public void runCurrentStep() {
-        if (steps.isEmpty()) {
-            return;
-        }
-        steps.get(0).run(this);
-    }
-
-    public void add(Step step) {
-        steps.add(step);
     }
 
 }
---
+++ mindstorms-group-2/src/MovementPrimitives.java
@@ -0,0 +1,6 @@
+
+public class MovementPrimitives {
+
+	public void fullSpeed() {
+	}
+}
