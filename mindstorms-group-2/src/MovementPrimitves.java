b94c5eff59230e3c1b58ec3b7106fb1d09356509
Mark Erhardt <al@alszacrel.de>
2015-01-27 16:21:30 +0100

    Created Variables for different motors

---
+++ mindstorms-group-2/src/MovementPrimitves.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+
+import lejos.nxt.Button;
+import lejos.nxt.LCD;
+import lejos.nxt.LightSensor;
+import lejos.nxt.SensorPort;
+
+public class MovementPrimitives {
+
+	public void fullSpeed() {
+	}
+}
