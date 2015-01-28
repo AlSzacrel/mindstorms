b8a0fe0b3350cc5fed99dcd9f501e68e65bc3d85
lars <briem.lars@googlemail.com>
2015-01-28 09:42:07 +0100

    Fixed compilation errors

--- mindstorms-group-2/src/MovementPrimitives.java
+++ mindstorms-group-2/src/MovementPrimitives.java
@@ -1,76 +1,66 @@
-import java.util.LinkedList;
-
-import lejos.nxt.Button;
-import lejos.nxt.LCD;
-import lejos.nxt.LightSensor;
-import lejos.nxt.SensorPort;
-
-import conf;
-
 public class MovementPrimitives {
-	private static final double CORRECTION_FACTOR = 0.75;
-	private int speed = 0;
-	public final Configuration conf;
-	
-	public MovementPrimitives(Configuration conf) 
-	{
-		this.conf = conf;
-	}
+    private static final float CORRECTION_FACTOR = 0.75f;
+    private float speed = 0;
+    public final Configuration conf;
 
-	public void fullSpeed() {
-		speed = Math.min(conf.getLeftWheel.getMaxSpeed(),conf.getRightWheel.getMaxSpeed());
-	}
+    public MovementPrimitives(Configuration conf) {
+        this.conf = conf;
+    }
 
-	public void slow() {
-		speed = 0.5*Math.min(conf.getLeftWheel.getMaxSpeed(),conf.getRightWheel.getMaxSpeed());
-	}
+    public void fullSpeed() {
+        speed = Math.min(conf.getLeftWheel().getMaxSpeed(), conf.getRightWheel().getMaxSpeed());
+    }
 
-	public void crawl() {
-		speed = 0.25*Math.min(conf.getLeftWheel.getMaxSpeed(),conf.getRightWheel.getMaxSpeed());
-	}
+    public void slow() {
+        speed = 0.5f * Math.min(conf.getLeftWheel().getMaxSpeed(), conf.getRightWheel().getMaxSpeed());
+    }
 
-	public void drive() {
-		conf.getLeftWheel.setSpeed(speed);
-		conf.getRightWheel.setSpeed(speed);
-	}
+    public void crawl() {
+        speed = 0.25f * Math.min(conf.getLeftWheel().getMaxSpeed(), conf.getRightWheel().getMaxSpeed());
+    }
 
-	public void backup() {
-		conf.getLeftWheel.setSpeed(-speed);
-		conf.getRightWheel.setSpeed(-speed);
-	}
+    public void drive() {
+        conf.getLeftWheel().setSpeed(speed);
+        conf.getRightWheel().setSpeed(speed);
+    }
 
-	public void stop() {
-		speed = 0;
-		drive();
-	}
+    public void backup() {
+        conf.getLeftWheel().setSpeed(-speed);
+        conf.getRightWheel().setSpeed(-speed);
+    }
 
-	public void correctionLeft() {
-		conf.getLeftWheel.setSpeed(CORRECTION_FACTOR*speed);
-		conf.getRightWheel.setSpeed(speed);
-	}
+    public void stop() {
+        speed = 0;
+        drive();
+    }
 
-	public void correctionRight() {
-		conf.getLeftWheel.setSpeed(speed);
-		conf.getRightWheel.setSpeed(CORRECTION_FACTOR*speed);
-	}
+    public void correctionLeft() {
+        conf.getLeftWheel().setSpeed(CORRECTION_FACTOR * speed);
+        conf.getRightWheel().setSpeed(speed);
+    }
 
-	public void turnLeft() {
-		conf.getLeftWheel.setSpeed(0);
-		conf.getRightWheel.setSpeed(speed);
-	}
+    public void correctionRight() {
+        conf.getLeftWheel().setSpeed(speed);
+        conf.getRightWheel().setSpeed(CORRECTION_FACTOR * speed);
+    }
 
-	public void turnRight() {
-		conf.getLeftWheel.setSpeed(speed);
-		conf.getRightWheel.setSpeed(0);
-	}
+    public void turnLeft() {
+        conf.getLeftWheel().setSpeed(0);
+        conf.getRightWheel().setSpeed(speed);
+    }
 
-	public void spinLeft() {
-		conf.getLeftWheel.setSpeed(-speed);
-		conf.getRightWheel.setSpeed(speed);
-	}
+    public void turnRight() {
+        conf.getLeftWheel().setSpeed(speed);
+        conf.getRightWheel().setSpeed(0);
+    }
 
-	public void spinRight() {
-		conf.getLeftWheel.setSpeed(speed);
-		conf.getRightWheel.setSpeed(-speed);
-	}
+    public void spinLeft() {
+        conf.getLeftWheel().setSpeed(-speed);
+        conf.getRightWheel().setSpeed(speed);
+    }
+
+    public void spinRight() {
+        conf.getLeftWheel().setSpeed(speed);
+        conf.getRightWheel().setSpeed(-speed);
+    }
 }
