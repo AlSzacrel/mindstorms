07529820bc3bc0b51d15644adfaa5845c146b656
Mark Erhardt <al@alszacrel.de>
2015-01-28 09:34:04 +0100

    Fixed errors in MovementPrimitives

--- mindstorms-group-2/src/MovementPrimitives.java
+++ mindstorms-group-2/src/MovementPrimitives.java
@@ -5,30 +5,38 @@ import lejos.nxt.LCD;
 import lejos.nxt.LightSensor;
 import lejos.nxt.SensorPort;
 
+import conf;
+
 public class MovementPrimitives {
-	private speed = 0;
 	private static final double CORRECTION_FACTOR = 0.75;
+	private int speed = 0;
+	public final Configuration conf;
+	
+	public MovementPrimitives(Configuration conf) 
+	{
+		this.conf = conf;
+	}
 
 	public void fullSpeed() {
-		speed = Math.min(leftWheel.getMaxSpeed(),rightWheel.getMaxSpeed());
+		speed = Math.min(conf.getLeftWheel.getMaxSpeed(),conf.getRightWheel.getMaxSpeed());
 	}
 
 	public void slow() {
-		speed = 0.5*Math.min(leftWheel.getMaxSpeed(),rightWheel.getMaxSpeed());
+		speed = 0.5*Math.min(conf.getLeftWheel.getMaxSpeed(),conf.getRightWheel.getMaxSpeed());
 	}
 
 	public void crawl() {
-		speed = 0.25*Math.min(leftWheel.getMaxSpeed(),rightWheel.getMaxSpeed());
+		speed = 0.25*Math.min(conf.getLeftWheel.getMaxSpeed(),conf.getRightWheel.getMaxSpeed());
 	}
 
 	public void drive() {
-		leftWheel.setSpeed(speed);
-		rightWheel.setSpeed(speed);
+		conf.getLeftWheel.setSpeed(speed);
+		conf.getRightWheel.setSpeed(speed);
 	}
 
 	public void backup() {
-		leftWheel.setSpeed(-speed);
-		rightWheel.setSpeed(-speed);
+		conf.getLeftWheel.setSpeed(-speed);
+		conf.getRightWheel.setSpeed(-speed);
 	}
 
 	public void stop() {
@@ -37,32 +45,32 @@ public class MovementPrimitives {
 	}
 
 	public void correctionLeft() {
-		leftWheel.setSpeed(CORRECTION_FACTOR*speed);
-		rightWheel.setSpeed(speed);
+		conf.getLeftWheel.setSpeed(CORRECTION_FACTOR*speed);
+		conf.getRightWheel.setSpeed(speed);
 	}
 
 	public void correctionRight() {
-		leftWheel.setSpeed(speed);
-		rightWheel.setSpeed(CORRECTION_FACTOR*speed);
+		conf.getLeftWheel.setSpeed(speed);
+		conf.getRightWheel.setSpeed(CORRECTION_FACTOR*speed);
 	}
 
 	public void turnLeft() {
-		leftWheel.setSpeed(0);
-		rightWheel.setSpeed(speed);
+		conf.getLeftWheel.setSpeed(0);
+		conf.getRightWheel.setSpeed(speed);
 	}
 
 	public void turnRight() {
-		leftWheel.setSpeed(speed);
-		rightWheel.setSpeed(0);
+		conf.getLeftWheel.setSpeed(speed);
+		conf.getRightWheel.setSpeed(0);
 	}
 
 	public void spinLeft() {
-		leftWheel.setSpeed(-speed);
-		rightWheel.setSpeed(speed);
+		conf.getLeftWheel.setSpeed(-speed);
+		conf.getRightWheel.setSpeed(speed);
 	}
 
 	public void spinRight() {
-		leftWheel.setSpeed(speed);
-		rightWheel.setSpeed(-speed);
+		conf.getLeftWheel.setSpeed(speed);
+		conf.getRightWheel.setSpeed(-speed);
 	}
 }
