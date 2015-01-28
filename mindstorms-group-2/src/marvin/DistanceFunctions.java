99e616fdef414c7bbeb4f102e316ac6bc7476fe9
vera-g <vera-g@is.naist.jp>
2015-01-28 18:22:09 +0100

    mostly informative comments for distances

--- mindstorms-group-2/src/marvin/DistanceFunctions.java
+++ mindstorms-group-2/src/marvin/DistanceFunctions.java
@@ -2,12 +2,20 @@ package marvin;
 
 public class DistanceFunctions implements Step {	
 
-	// TODO: alle theshholds mit Daten anpassen
+	/*
+	 * Perceptable Data Range: [3;31]
+	 * 3 is hend in front of sensor
+	 * meassured values up to 60, but not on the stupid plane!!!
+	 * Set head back on 28.01. ~17h
+	 * Before that: distance to wall when touching: 9~10
+	 */
 	private static final int SIDE_WALL_THRESH = 35;
 	private static final int FRONT_WALL_THRESH = 18; //average 24.2
-	private static final int FOLLOW_WALL_THRESH = 18;
+	private static final int FOLLOW_WALL_THRESH = 20;
 	private static final int SIDE_EDGE_THRESH = 100; //Können wir das überhaupt noch wahrnehmen?
 	private static final int FRONT_EDGE_THRESH = 18; 
+	private static final int WALL_TOO_CLOSE_THRESH = 15;
+	
     private DataSet sensData;
 
 	@Override
