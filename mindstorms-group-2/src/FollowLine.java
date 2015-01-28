83dc06cfa846c2704c3e38b30afc6ea5dc72da37
vera-g <vera-g@is.naist.jp>
2015-01-28 14:20:43 +0100

    removed old commented cote

--- mindstorms-group-2/src/FollowLine.java
+++ mindstorms-group-2/src/FollowLine.java
@@ -100,27 +100,4 @@ public class FollowLine implements Step {
         }
         return currentCase;
     }
-
-    // alter Code der mit schiebesensor nicht funktioniert: nur für Referenz
-
-    /**
-     * private enum Direction { LEFT() {
-     *
-     * @Override public void switchDirection() { Motor.B.stop();
-     *           Motor.A.forward(); } }, RIGHT() {
-     * @Override public void switchDirection() { Motor.A.stop();
-     *           Motor.B.forward(); } };
-     *
-     *           public abstract void switchDirection(); }
-     *
-     *           private static final int SILVER_LINE_THRESHOLD = 375;
-     *
-     *           private Direction last;
-     * @Override public void run(Configuration configuration) { Direction
-     *           currentDirection =
-     *           configuration.getLight().getNormalizedLightValue() <
-     *           SILVER_LINE_THRESHOLD ? Direction.LEFT : Direction.RIGHT; if
-     *           (currentDirection == last) { return; }
-     *           currentDirection.switchDirection(); last = currentDirection; }
-     */
 }
