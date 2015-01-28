4ba0af9828d810dddb02c71353ee7374c5294743
vera-g <vera-g@is.naist.jp>
2015-01-28 16:40:31 +0100

    small fixes & adaptions

--- mindstorms-group-2/src/DistanceFunctions.java
+++ mindstorms-group-2/src/DistanceFunctions.java
@@ -1,9 +1,11 @@
 
 public class DistanceFunctions implements Step {	
 
-	private static final int SIDE_WALL_THRESH = 50;
+	// TODO: alle theshholds mit Daten anpassen
+	private static final int SIDE_WALL_THRESH = 35;
 	private static final int FRONT_WALL_THRESH = 18; //average 24.2
-	private static final int SIDE_EDGE_THRESH = 50;
+	private static final int FOLLOW_WALL_THRESH = 18;
+	private static final int SIDE_EDGE_THRESH = 100; //Können wir das überhaupt noch wahrnehmen?
 	private static final int FRONT_EDGE_THRESH = 18; 
     private DataSet sensData;
 
@@ -13,14 +15,14 @@ public class DistanceFunctions implements Step {
 		
 	}
 
-	public boolean isWallLeft() {
+	private boolean isWallLeft() {
 		if (sensData.get(0).getDistance() < SIDE_WALL_THRESH) {
 			return true;
 		}
 		return false;
 	}
 
-	public boolean isWallRight() {
+	private boolean isWallRight() {
 		if (sensData.get(sensData.size() - 1).getDistance() < SIDE_WALL_THRESH) {
 			return true;
 		}
@@ -28,7 +30,7 @@ public class DistanceFunctions implements Step {
 		
 	}
 
-	public boolean isWallUpFront() {
+	private boolean isWallUpFront() {
 		if (sensData.get(sensData.size() / 2).getDistance() < FRONT_WALL_THRESH) {
 			return true;
 		}
@@ -36,14 +38,14 @@ public class DistanceFunctions implements Step {
 		
 	}
 
-	public boolean isEdgeLeft() {
+	private boolean isEdgeLeft() {
 		if (sensData.get(0).getDistance() < SIDE_EDGE_THRESH) {
 			return true;
 		}
 		return false;
 	}
 
-	public boolean isEdgeRight() {
+	private boolean isEdgeRight() {
 		if (sensData.get(sensData.size() - 1).getDistance() < SIDE_EDGE_THRESH) {
 			return true;
 		}
@@ -51,11 +53,17 @@ public class DistanceFunctions implements Step {
 		
 	}
 
-	public boolean isEdgeUpFront() {
+	private boolean isEdgeUpFront() {
 		if (sensData.get(sensData.size() / 2).getDistance() < FRONT_EDGE_THRESH) {
 			return true;
 		}
-		return false;
-		
-	}	
+		return false;		
+	}
+	
+	// needs to be called from the outside
+	private void followLeftWall() {
+		if (isWallLeft()  && sensData.get(0).getDistance() < FOLLOW_WALL_THRESH) {
+			
+		}
+	}
 }
