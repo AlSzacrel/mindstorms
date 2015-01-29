f6b694bf63003eb43bd30a47f4b0f230670736a9
wei quan <wei.quan@restaurant-cards.de>
2015-01-29 17:38:24 +0100

    added minor filter change

--- mindstorms-group-2/src/marvin/Filter.java
+++ mindstorms-group-2/src/marvin/Filter.java
@@ -6,7 +6,7 @@ public class Filter {
 	private static final float ALPHA = 0.125f;
 
 	// exponential weighted moving average
-	public float avgEWMA(float estimate, int sample){
+	public static float avgEWMA(float estimate, int sample){
 		
 	    return (1-ALPHA)*estimate + ALPHA*sample;
 	}
