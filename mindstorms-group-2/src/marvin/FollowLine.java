0e2a6620cc092af4e7934006809affd5ec40b47b
lars <briem.lars@googlemail.com>
2015-01-29 18:15:17 +0100

    Spin left/right when line is long (orthogonal)

--- mindstorms-group-2/src/marvin/FollowLine.java
+++ mindstorms-group-2/src/marvin/FollowLine.java
@@ -69,6 +69,21 @@ public class FollowLine implements Step {
             public void adjustCourse(MovementPrimitives movPrim) {
                 movPrim.correctionRight();
             }
+        },
+        SPIN_LEFT() {
+
+            @Override
+            public void adjustCourse(MovementPrimitives movPrim) {
+                movPrim.spinLeft();
+            }
+        },
+
+        SPIN_RIGHT() {
+
+            @Override
+            public void adjustCourse(MovementPrimitives movPrim) {
+                movPrim.spinRight();
+            }
         };
 
         public abstract void adjustCourse(MovementPrimitives movPrim);
@@ -79,11 +94,11 @@ public class FollowLine implements Step {
         StraightCase currentCase = null;
         LineBorders lineBorders = config.getLines().get(config.getLines().size() - 1);
         int rightBorder = lineBorders.getBrightToDark();
-		int leftBorder = lineBorders.getDarkToBright();
-		int lineWidth = rightBorder - leftBorder;
-        float lineCenter = (rightBorder + leftBorder)/2;
-        
-		if (rightBorder == Integer.MIN_VALUE && leftBorder == Integer.MIN_VALUE) {
+        int leftBorder = lineBorders.getDarkToBright();
+        int lineWidth = rightBorder - leftBorder;
+        float lineCenter = (rightBorder + leftBorder) / 2;
+
+        if (rightBorder == Integer.MIN_VALUE && leftBorder == Integer.MIN_VALUE) {
             currentCase = StraightCase.LOST;
         } else if (rightBorder == Integer.MIN_VALUE) {
             currentCase = StraightCase.TURN_RIGHT;
@@ -91,10 +106,17 @@ public class FollowLine implements Step {
             currentCase = StraightCase.TURN_LEFT;
         } else if (lineWidth > 90) { // TODO: How wide is the line?
             // Line is too wide, might be orthogonal line or corner.
-            currentCase = StraightCase.ORTHOGONAL;
+            // TODO what is with long lines which have both ends?
+            if (leftBorder >= 115) {
+                // Line is to the right of center
+                currentCase = StraightCase.SPIN_RIGHT;
+            } else {
+                // Line is to the left of center
+                currentCase = StraightCase.SPIN_LEFT;
+            }
         } else if (lineWidth > 0) {
             // We're still on the line.
-        	if (64 < lineCenter && lineCenter < 94) {
+            if (64 < lineCenter && lineCenter < 94) {
                 // Line is centrally in front of us.
                 currentCase = StraightCase.STRAIGHT;
             } else if (lineCenter >= 94) {
