56e0514832ca3d40b66ed15f98e4977c3cb26d78
lars <briem.lars@googlemail.com>
2015-01-30 16:03:36 +0100

    Introduced remote debugging

--- mindstorms-group-2/src/marvin/Marvin.java
+++ mindstorms-group-2/src/marvin/Marvin.java
@@ -2,37 +2,35 @@ package marvin;
 
 import java.io.IOException;
 
-import lejos.nxt.comm.RConsole;
-
 public class Marvin {
 
-	private boolean running = true;
-	private final Configuration configuration;
+    private boolean running = true;
+    private final Configuration configuration;
 
-	public Marvin(Configuration configuration) {
-		this.configuration = configuration;
-	}
+    public Marvin(Configuration configuration) {
+        this.configuration = configuration;
+    }
 
-	public void drive() throws IOException {
-		 RConsole.openUSB(0);
-		// RConsole.println("collect data");
-		// RConsole.println(configuration.getLines().toString());
-		while (running) {
-			cancelRun();
-			configuration.displayInformation();
-			configuration.getSensorDataCollector().collectData();
-			// configuration.getMovementPrimitives().slow();
-			// configuration.getMovementPrimitives().drive();
-			configuration.followLine();
-			// Delay.msDelay(500);
-			// configuration.getMovementPrimitives().stop();
-		}
-		configuration.save();
-	}
+    public void drive() throws IOException {
+        configuration.startConsole();
+        // RConsole.println("collect data");
+        // RConsole.println(configuration.getLines().toString());
+        while (running) {
+            cancelRun();
+            configuration.displayInformation();
+            configuration.getSensorDataCollector().collectData();
+            // configuration.getMovementPrimitives().slow();
+            // configuration.getMovementPrimitives().drive();
+            configuration.followLine();
+            // Delay.msDelay(500);
+            // configuration.getMovementPrimitives().stop();
+        }
+        configuration.save();
+    }
 
-	private void cancelRun() {
-		if (configuration.isCancel()) {
-			running = false;
-		}
-	}
+    private void cancelRun() {
+        if (configuration.isCancel()) {
+            running = false;
+        }
+    }
 }
