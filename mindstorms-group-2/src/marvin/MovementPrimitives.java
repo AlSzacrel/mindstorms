4d3ba838b6260ff1b87deb61ca019029a1887c01
lars <briem.lars@googlemail.com>
2015-02-05 10:49:56 +0100

    Improved reel board handling

--- mindstorms-group-2/src/marvin/MovementPrimitives.java
+++ mindstorms-group-2/src/marvin/MovementPrimitives.java
@@ -4,11 +4,12 @@ public class MovementPrimitives {
     private static final float CORRECTION_FACTOR = 0.75f;
     private static final float TURN_FACTOR = 0.5f;
     private static final float SPIN_FACTOR = 1f;
-    private static final float SPEED_FACTOR_FULL = 0.5f;
-    private static final float BACKUP_FACTOR = SPEED_FACTOR_FULL / 2f;
-    private static final float SPEED_FACTOR_SLOW = SPEED_FACTOR_FULL / 2f;
+    private static final float SPEED_FACTOR_REEL = 0.75f;
+    private static final float SPEED_FACTOR_NORMAL = 0.5f;
+    private static final float BACKUP_FACTOR = SPEED_FACTOR_NORMAL / 2f;
+    private static final float SPEED_FACTOR_SLOW = SPEED_FACTOR_NORMAL / 2f;
     private static final float SPEED_FACTOR_CRAWL = SPEED_FACTOR_SLOW / 2f;
-	private static final float SPEED_FACTOR_STALK = SPEED_FACTOR_CRAWL / 4f;
+    private static final float SPEED_FACTOR_STALK = SPEED_FACTOR_CRAWL / 4f;
     private float speed = 0;
     public final Configuration conf;
 
@@ -18,7 +19,15 @@ public class MovementPrimitives {
     }
 
     public void fullSpeed() {
-        speed = SPEED_FACTOR_FULL * Math.min(conf.getLeftWheel().getMaxSpeed(), conf.getRightWheel().getMaxSpeed());
+        speed = Math.min(conf.getLeftWheel().getMaxSpeed(), conf.getRightWheel().getMaxSpeed());
+    }
+
+    public void reelSpeed() {
+        speed = SPEED_FACTOR_REEL * Math.min(conf.getLeftWheel().getMaxSpeed(), conf.getRightWheel().getMaxSpeed());
+    }
+
+    public void normalSpeed() {
+        speed = SPEED_FACTOR_NORMAL * Math.min(conf.getLeftWheel().getMaxSpeed(), conf.getRightWheel().getMaxSpeed());
     }
 
     public void slow() {
@@ -110,8 +119,8 @@ public class MovementPrimitives {
         conf.getRightWheel().setSpeed(speed);
     }
 
-	public void searchBarcode() {
-		// TODO Auto-generated method stub
-		
-	}
+    public void searchBarcode() {
+        // TODO Auto-generated method stub
+
+    }
 }
--- mindstorms-group-2/src/marvin/ReelBoard.java
+++ mindstorms-group-2/src/marvin/ReelBoard.java
@@ -1,5 +1,9 @@
 package marvin;
 
+import lejos.nxt.LightSensor;
+import lejos.nxt.NXTRegulatedMotor;
+import lejos.nxt.UltrasonicSensor;
+import lejos.nxt.comm.RConsole;
 import lejos.util.Delay;
 
 import communication.BluetoothCommunication;
@@ -7,25 +11,58 @@ import communication.GateConnection;
 
 public class ReelBoard implements Step {
 
+    private static final int SIDE_WALL_THRESHOLD = 8;
+    private static final int GAIN = 2;
+    private static final int MAX_CORRECTION = 100;
+    private static final int MIN_CORRECTION = -MAX_CORRECTION;
+
     @Override
     public void run(Configuration configuration) {
         configuration.getMovementPrimitives().stop();
-        configuration.getLeftWheel().rotate(-200, true);
-        configuration.getRightWheel().rotate(-200, true);
-        configuration.getLeftWheel().waitComplete();
-        configuration.getRightWheel().waitComplete();
+        configuration.getMovementPrimitives().reelSpeed();
+        configuration.getMovementPrimitives().resetSpeed();
+        configuration.getSensorDataCollector().turnToWallPosition();
+        // DifferentialPilot pilot = new
+        // DifferentialPilot(DifferentialPilot.WHEEL_SIZE_RCX,
+        // DifferentialPilot.WHEEL_SIZE_RCX * 2, configuration.getLeftWheel(),
+        // configuration.getRightWheel());
+        // pilot.setTravelSpeed(pilot.getMaxTravelSpeed());
+        NXTRegulatedMotor leftWheel = configuration.getLeftWheel();
+        NXTRegulatedMotor rightWheel = configuration.getRightWheel();
         try (GateConnection gate = BluetoothCommunication.connectToGate(configuration)) {
-            configuration.getMovementPrimitives().fullSpeed();
-            configuration.getMovementPrimitives().drive();
-            Delay.msDelay(5000);
-            configuration.getMovementPrimitives().stop();
+            leftWheel.rotate(1600, true);
+            rightWheel.rotate(1600, true);
+            SensorDataCollector sensorDataCollector = configuration.getSensorDataCollector();
+            LightSensor light = configuration.getLight();
+            boolean lineDetected = false;
+            while (!configuration.isCancel() && !lineDetected && leftWheel.isMoving()) {
+                followWall(configuration.getMovementPrimitives(), configuration.getUltraSonic());
+                // lineDetected =
+                // sensorDataCollector.isBright(light.getNormalizedLightValue());
+            }
+            leftWheel.stop();
+            rightWheel.stop();
+
+            // TODO watch distance
+            // Delay.msDelay(5000);
+            // configuration.getMovementPrimitives().stop();
             gate.passed();
             while (!gate.waitForSuccess()) {
                 Delay.msDelay(50);
             }
             System.out.println("Successful passed");
         }
+    }
 
+    private void followWall(MovementPrimitives movement, UltrasonicSensor ultraSonic) {
+        int distance = ultraSonic.getDistance();
+        int correctionFactor = SIDE_WALL_THRESHOLD - distance;
+        int gainedCorrection = correctionFactor * GAIN;
+        int limittedCorrection = Math.max(gainedCorrection, MIN_CORRECTION);
+        limittedCorrection = Math.min(limittedCorrection, MAX_CORRECTION);
+        RConsole.println("d: " + distance + " c: " + correctionFactor + " g: " + gainedCorrection + " l: "
+                + limittedCorrection);
+        movement.correct(limittedCorrection);
     }
 
     @Override
